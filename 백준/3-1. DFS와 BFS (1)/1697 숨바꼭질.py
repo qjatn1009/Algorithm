224e91077f0d10603e593c498c9bd1dec7d7804c
qjatn1009 <a43727702@gmail.com>
2020-09-08 02:35:11 +0900

    DFS, BFS

---
+++ 백준/3-1. DFS와 BFS (1)/1697 숨바꼭질.py
@@ -0,0 +1,25 @@
+#보고 공부좀 더할것..
+N, K = map(int, input().split())
+
+if N >= K:
+    print(N-K)
+else:
+    check = [0 for i in range(K+2)]
+
+    move_list = [N]
+    check[N] = 1
+    while move_list:
+        move = move_list.pop(0)
+        if move == K:
+            result = check[K]-1
+            break
+        if move + 1 <= K+1 and check[move+1] == 0:
+            check[move+1] = check[move] + 1
+            move_list.append(move+1)
+        if move - 1 >= 0 and check[move-1] == 0:
+            check[move-1] = check[move] + 1
+            move_list.append(move-1)
+        if move*2 <= K+1 and check[move*2] == 0:
+            check[move*2] = check[move] + 1
+            move_list.append(move*2)
+    print(result)
