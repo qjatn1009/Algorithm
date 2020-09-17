bab2b132fd6e7e0e8d98acf5e6cc36027104c341
qjatn1009 <a43727702@gmail.com>
2020-09-17 17:58:25 +0900

    Level 2

--- 프로그래머스/Level 2/땅따먹기.py
+++ 프로그래머스/Level 2/땅따먹기.py
@@ -1,25 +1,12 @@
-# DP 문제 , 틀림
 def solution(land):
     answer = 0
-    current, cnt = land[0].index(max(land[0])), 0
-    while cnt < len(land):
 
-        if current == land[cnt+1].index(max(land[cnt+1])):
-            if sorted(land[cnt])[-1] + sorted(land[cnt+1])[-2] > sorted(land[cnt])[-2] + sorted(land[cnt+1])[-1]:
-                answer += sorted(land[cnt])[-1] + sorted(land[cnt+1])[-2]
-                current = land[cnt+1].index(sorted(land[cnt+1])[-2])
-                cnt += 2
-            else:
-                answer += sorted(land[cnt])[-2] + sorted(land[cnt+1])[-1]
-                current = land[cnt+1].index(sorted(land[cnt+1])[-1])
-                cnt += 2
-        else:
-            answer += max(land[cnt])
-            current = land[cnt].index(max(land[cnt]))
-            cnt += 1
-        
-     
+    for i in range(1, len(land)):
+        for j in range(len(land[i])):
+            land[i][j] += max(land[i-1][:j]+land[i-1][j+1:])
+
+    answer = max(land[-1])
 
     return answer
 
-print(solution(	[[1, 2, 3, 5], [10, 11, 12, 11], [16, 15, 13, 13]]))
+print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
