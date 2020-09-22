0e29f554091a500d48f9e6498fb6093aa65e948f
qjatn1009 <a43727702@gmail.com>
2020-09-22 22:25:00 +0900

    다이나믹 프로그래밍

---
+++ 백준/다이나믹프로그래밍/1937 욕심쟁이 판다.py
@@ -0,0 +1,23 @@
+def dfs(i, j):
+    if memo[i][j]:
+        return memo[i][j]
+    memo[i][j] = 1
+    for a in range(4):
+        ni, nj = i+di[a], j+dj[a]
+        if 0 <= ni < n and 0 <= nj < n and forest[i][j] < forest[ni][nj]:
+            memo[i][j] = max(memo[i][j], dfs(ni, nj)+1)
+    return memo[i][j]
+
+n = int(input())
+forest = [list(map(int, input().split())) for _ in range(n)]
+memo = [[0] * n for _ in range(n)]
+di = [-1,1,0,0]
+dj = [0,0,-1,1]
+# 아래 위 왼 오
+for i in range(n):
+    for j in range(n):
+        memo[i][j] = dfs(i,j)
+day = 0
+for i in memo:
+    day = max(day, max(i))
+print(day)
