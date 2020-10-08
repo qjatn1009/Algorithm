880127bfbba20847241f85bdd7fa6d7cd3cf8cc0
qjatn1009 <a43727702@gmail.com>
2020-10-08 18:56:22 +0900

    Level 2

--- 프로그래머스/Level 2/가장 큰 정사각형 찾기.py
+++ 프로그래머스/Level 2/가장 큰 정사각형 찾기.py
@@ -1,28 +1,22 @@
-# 효율성 문제
-def solution(board):
-    answer = [0]
-    possible = False
-    m = min(len(board), len(board[0]))
-    result = [ i for i in range(m,0,-1)]
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j] == 1:
-                for k in result:
-                    if k + i <= len(board) and k + j <= len(board[i]):
-                        if square(i, j, k, board):
-                            answer.append(k*k)
-                            result = result[:result.index(k)]
-                            break
-    print(answer)
-    return max(answer)
-
-def square(row, column, N, board):
-    for i in range(N):
-        for j in range(N):
-            if board[row+i][column+j] != 1:
-                return False
-    return True
-
+# DP 문제
+def solution(board): # 전체 검사하면 무조건 시간 초과
+    answer = 0
+    ny, nx = len(board), len(board[0])
+    for y in range(ny):
+        for x in range(nx):
+            if board[y][x] == 1:
+                answer = 1
+    for y in range(1, ny):
+        for x in range(1, nx):
+            if board[y][x] == 0:
+                continue
+            else:
+                if board[y-1][x]*board[y-1][x-1]*board[y][x-1] >0 : 
+                    board[y][x] = min(board[y-1][x], board[y-1][x-1], board[y][x-1]) + 1
+                    if answer < board[y][x]:
+                        answer = board[y][x]
+    answer *= answer
+    return answer
 
 print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
 print(solution([[0,0,1,1],[1,1,1,1]]))
