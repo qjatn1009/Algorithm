cce9ffb2eaed77f5355d386efa42df688776f056
qjatn1009 <a43727702@gmail.com>
2020-10-12 15:56:43 +0900

    다이나믹 프로그래밍

---
+++ 백준/BOJ 길라잡이/5-2. 다이나믹 프로그래밍(1)/1520 내리막 길.py
@@ -0,0 +1,25 @@
+from collections import deque
+m, n = map(int, input().split())
+maps = []
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+for i in range(m):
+    maps.append(list(map(int, input().split())))
+dp = [[-1 for _ in range(n)] for _ in range(m)]
+
+def dfs(ny, nx):    
+
+    if nx == n-1 and ny == m-1:
+        return 1
+    if dp[ny][nx] != -1:
+        return dp[ny][nx]
+    dp[ny][nx] = 0
+    for i in range(4):
+        y = ny + dy[i]
+        x = nx + dx[i]
+        if 0 <= y < m and 0 <= x < n:
+            if maps[y][x] < maps[ny][nx]:
+                dp[ny][nx] += dfs(y, x)
+    return dp[ny][nx]
+
+print(dfs(0, 0))
